2a62dc8e9f3ed5c0b3e6858e4ea5e585a7c27eaf
Ismayil Mirzayev <ismayil.mirzayev0@gmail.com>
2021-11-11 20:18:03 +0400

    Initial commit

---
+++ resources/views/front/partials/header.blade.php
@@ -0,0 +1,43 @@
+<header>
+    <div class="container">
+        <div class="header">
+            <div class="header__left">
+                <div class="header__logo">
+                    <a href="">Easy</a>
+                </div>
+                <nav class="header__nav">
+                    <ul>
+                        <li>
+                            <a href="">Home</a>
+                        </li>
+                        <li>
+                            <a href="">Home</a>
+                        </li>
+                        <li>
+                            <a href="">Home</a>
+                        </li>
+                        <li>
+                            <a href="">Home</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="header__right">
+                <a href="">Login</a>
+            </div>
+        </div>
+        <div class="hero">
+            <div class="hero__left">
+                <h1 class="hero__title">Get Paid Easily <br> without Hassle</h1>
+                <p class="hero__text">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live.</p>
+                <form action="">
+                    <div class="form__box">
+                        <input type="text" placeholder="Email">
+                        <button>Subsribe</button>
+                    </div>
+                </form>
+            </div>
+            <div class="hero__right"></div>
+        </div>
+    </div>
+</header>
